2708f2b75a248b6e55818de52d71659cf9c26a5b
sistum <sistum@elsevier.com>
2020-12-17 09:03:31 +0000

    Used awaitility library

--- src/test/java/com/elsevier/usage/apitests/utils/JsonReaderUtils.java
+++ src/test/java/com/elsevier/usage/apitests/utils/JsonReaderUtils.java
@@ -1,26 +1,21 @@
 package com.elsevier.usage.apitests.utils;
 
 
-
-
-import org.json.simple.parser.JSONParser;
-
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
+
 import java.io.FileReader;
 import java.io.IOException;
 
 public class JsonReaderUtils {
     public static String readJson(String filePath) throws IOException {
-        JSONParser parser = new JSONParser();
         StringBuilder stringBuilder = new StringBuilder();
-        FileReader fr= new FileReader(filePath);
+        FileReader fr = new FileReader(filePath);
         BufferedReader br = new BufferedReader(fr);
-        String line= null;
-        while ((line = br.readLine()) != null){
+        String line = null;
+        while ((line = br.readLine()) != null) {
             stringBuilder.append(line);
         }
-        System.out.println("------------------->"+stringBuilder.toString());
+        System.out.println("------------------->" + stringBuilder.toString());
         return stringBuilder.toString();
     }
 
